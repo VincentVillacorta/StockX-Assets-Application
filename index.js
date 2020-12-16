c09dcd6dab041135b3673a3d6a2f0388242f0a7c
Vince <vincentjvillacorta@gmail.com>
2020-12-16 12:52:46 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const StockXAPI = require('stockx-api')
+
+const app = express()
+const stockX = new StockXAPI()
+
+
+const search = process.argv[2]
+
+stockX.newSearchProducts(search, {
+    limit:10
+}).then((products) => {
+    products.forEach((product) => {
+        console.log(product.name)
+    })
+})
+.catch(err => console.log('Error', err))
