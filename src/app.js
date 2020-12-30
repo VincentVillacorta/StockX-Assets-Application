87067ddeca96022bf7fbeddb591d3e97c7eace65
Vince <vincentjvillacorta@gmail.com>
2020-12-30 14:47:22 -0800

    Temporary access to local host

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const userRouter = require('./routers/user')
 const app = express()
 
 app.use(express.json())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(session({secret: process.env.SECRET}));
 app.use(userRouter)
 
