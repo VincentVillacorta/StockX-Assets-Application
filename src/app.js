a375f575b0523b04d07c19c9d6f0f9715ed03f0d
Vince <vincentjvillacorta@gmail.com>
2021-01-20 15:37:57 -0800

    updated CORS

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://stock-x-assets.herokuapp.com/',
     credentials: true
 }))
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://stock-x-assets.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
--- src/stock-x/stock-x-functions.js
+++ src/stock-x/stock-x-functions.js
@@ -2,7 +2,7 @@ const StockXAPI = require('stockx-api')
 const stockX = new StockXAPI()
 
 const searchItem = async (name, id) => {
-    return await stockX.newSearchProducts(name, {limit: 5})
+    return await stockX.newSearchProducts(name, {limit: 6})
 };
 
 const getFullItem = async (name, id) => {
