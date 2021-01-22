b0389bebcb096b3b5cf994f27d0f09501ba4d77e
Vince <vincentjvillacorta@gmail.com>
2021-01-22 13:42:11 -0800

    no cors middleware

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const session = require('express-session');
-const cors = require("cors");
 require("./db/mongoose")
 const userRouter = require('./routers/user')
 
 const app = express()
 
 app.use(express.json())
-// app.use(cors({
-//     origin:'https://stock-x-assets.herokuapp.com/',
-//     credentials: true
-// }))
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', 'https://stock-x-assets.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
