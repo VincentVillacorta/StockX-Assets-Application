5ac55f83fe5d319c979a7e193c7653af62b69432
Vince <vincentjvillacorta@gmail.com>
2021-01-22 13:50:40 -0800

    local

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://stock-x-assets.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
